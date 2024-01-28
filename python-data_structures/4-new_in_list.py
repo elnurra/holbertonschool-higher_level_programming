2a6dec277aa67901996d63abdf9f589de02db093
Elnurra <elnurra@code.edu.az>
2024-01-28 06:04:24 -0800

    feat: trying to fix code

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,6 +3,8 @@
 Write a function that replaces an element in a list at a specific
 position without modifying the original list
 """
+
+
 def new_in_list(my_list, idx, element):
     copy = my_list[0:]
     count = len(my_list)
