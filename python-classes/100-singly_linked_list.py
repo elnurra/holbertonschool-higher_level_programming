327626423ecd0c3f3a8aaeffaa4f0507cc7bead4
Elnurra <elnurra@code.edu.az>
2024-02-03 04:32:27 -0800

    feat: added  a class Node that defines a node of a singly linked list by:

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python3
 class Node:
+    """Node class for a singly linked list."""
     def __init__(self, data, next_node=None):
+        """Initialize a Node with data and an optional next_node."""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Getter for the data attribute."""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """Setter for the data attribute."""
         if not isinstance(value, int):
             raise TypeError('data must be an integer')
         self.__data = value
 
     @property
     def next_node(self):
+        """Getter for the next_node attribute."""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """Setter for the next_node attribute."""
         if value is not None and type(value) != Node:
             raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """Singly Linked List class."""
     def __init__(self):
+        """Initialize an empty Singly Linked List."""
         self.head = None
 
     def __str__(self):
+        """Return a string representation of the linked list."""
         ret = ""
         node = self.head
         while node:
@@ -38,6 +47,7 @@ class SinglyLinkedList:
         return ret[:-1]
 
     def sorted_insert(self, value):
+        """Insert a new node with the given value in sorted order."""
         new = Node(value)
         if not self.head:
             self.head = new
@@ -54,3 +64,4 @@ class SinglyLinkedList:
         if node.next_node:
             new.next_node = node.next_node
         node.next_node = new
+
