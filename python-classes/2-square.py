1ebf6ff1be27f20f51326037d34cd04abbd38cb2
Elnurra <elnurra@code.edu.az>
2024-02-03 03:20:06 -0800

    feat: added a class Square that defines a square by: (based on 1-square.py)

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-class Square:
-    """this is a class Square that defines a square
-    Attributes:
-       __size: Private instance attribute - size of square
-    Args:
-       size: size of square.
-    """
-    def __init__(self, size=0):
-        """This is a function that use __init__ method to
-        initialize the passing variables
+"""a class Square that defines a square by: (based on 2-square.py)"""
 
-        Attributes:
-             __size: Private instance attribute - size of square
-        Args:
-             size: size of square
-        Raises:
-             TypeError: size must be an integer
-             ValueError: size must be >= 0
-        """
-        if isinstance(size, int):
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError("size must be >= 0")
-        else:
+
+class Square:
+    """a class that initialiazes an object with an attribute"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
