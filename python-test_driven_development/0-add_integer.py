2ce19b2fddaa639d7e2f4ea0447e609915c1bb84
Elnurra <elnurra@code.edu.az>
2024-02-04 03:45:29 -0800

    feat: added module documematation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module to find the max integer in a list
+"""
+
+
 def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
