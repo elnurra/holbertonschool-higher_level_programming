f61c60bb206ee4abfa7479da1a8f926c193f8678
Elnurra <elnurra@code.edu.az>
2024-02-04 03:31:58 -0800

    feat: added some integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
 """
-Module to find the max integer in a list
+    Insert here module comment
 """
 
 
 def add_integer(a, b=98):
-    """ This is a function that add two integers
-    Args:
-    a: first integer that need to add
-    b: second integer that need to add
-    Return:
-    return: sum
-    """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
-    if a is None or not isinstance(a, (int, float)):
+    """ add two integers or flots """
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    sum = a + b
-    if sum == float('inf') or sum == -float('inf'):
-        return 89
-    return int(a) + int(b)
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testfile("./tests/0-add_integer.txt")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return (a + b)
