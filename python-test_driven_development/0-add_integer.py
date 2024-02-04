25e8326a9f4ac0fa81d1398b381bbac1674c0a97
Elnurra <elnurra@code.edu.az>
2024-02-04 03:47:24 -0800

    feat: added function documanation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,6 +5,13 @@ Module to find the max integer in a list
 
 
 def add_integer(a, b=98):
+    """ This is a function that add two integers
+    Args:
+    a: first integer that need to add
+    b: second integer that need to add
+    Return:
+    return: sum
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
