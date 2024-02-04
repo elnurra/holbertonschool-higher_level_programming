3a082cb1e674f41d1374afce6aee94df561ee779
Elnurra <elnurra@code.edu.az>
2024-02-04 05:58:44 -0800

    feat: added some codes

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -15,10 +15,19 @@ def lazy_matrix_mul(m_a, m_b):
         numpy.ndarray: The result of the matrix multiplication.
 
     Raises:
-        ValueError: If either m_a or m_b is a string, as matrix multiplication is not defined for strings.
+        ValueError: If either m_a or m_b is a string,
+        as matrix multiplication is not defined for strings.
     """
-    if isinstance(m_a, str) or isinstance(m_b, str):
-        raise ValueError("Matrix multiplication is not defined for a string.")
+    if isinstance(m_a, str):
+        if isinstance(m_b, np.ndarray):
+            return np.array([m_a * element for element in m_b], dtype=object)
+        else:
+            raise ValueError("Matrix multiplication is not defined for a string.")
+    elif isinstance(m_b, str):
+        if isinstance(m_a, np.ndarray):
+            return np.array([element * m_b for element in m_a], dtype=object)
+        else:
+            raise ValueError("Matrix multiplication is not defined for a string.")
     else:
         return np.matmul(m_a, m_b)
 
