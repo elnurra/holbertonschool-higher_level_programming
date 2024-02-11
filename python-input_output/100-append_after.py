08490deb149135d0b87fe238ce030a70d6ce719a
Elnur <elnurra@code.edu.az>
2024-02-11 09:09:37 -0800

    bug-fix gor pep

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-function that inserts a line of text to a file, after each line containing a specific string
-"""
+"""Search and update"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    '''module Search and update
-    '''
-    with open(filename, 'r+') as f:
-        lines = f.readlines()
-        i = 0
-        for line in lines:
-            if line.find(search_string) is not -1:
-                lines.insert(i + 1, new_string)
-            i += 1
-        f.seek(0)
-        f.write("".join(lines))
+    """inserts a line of text to a file, after each line with a spc. string"""
+    text = ""
+    with open(filename, "r") as file:
+        for line in file:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as file:
+        file.write(text)
