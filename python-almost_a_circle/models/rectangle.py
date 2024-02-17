8d2a932d20c24066413a2401c855cc75eea6618d
Elnur <elnurra@code.edu.az>
2024-02-17 10:05:22 -0800

    feat: added the class Rectangle by updating the public method def update(self, *args): by changing the prototype to update(self, *args, **kwargs) that assigns a key/value argument to attributes:

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -93,15 +93,12 @@ class Rectangle(Base):
             self.id, self.__x, self.__y, self.__width, self.__height
         )
 
-    def update(self, *args):
-        """Assigns arguments to attributes in a specific order."""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        """Assigns key/value arguments to attributes."""
+        if args:
+            attrs = ['id', 'width', 'height', 'x', 'y']
+            for attr, val in zip(attrs, args):
+                setattr(self, attr, val)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
