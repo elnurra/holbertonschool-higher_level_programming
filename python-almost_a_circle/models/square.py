06b1b8f410d36ba9d0d6d3fbe6d43fc506f98cef
Elnur <elnurra@code.edu.az>
2024-02-17 10:12:43 -0800

    feat: added the class Square by adding the public getter and setter size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Defines a Square class that inherits from Rectangle."""
 
-
 from models.rectangle import Rectangle
-
 
 class Square(Rectangle):
     """Square class that inherits from Rectangle."""
@@ -11,6 +9,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes a Square instance."""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Returns a string representation of the Square instance."""
