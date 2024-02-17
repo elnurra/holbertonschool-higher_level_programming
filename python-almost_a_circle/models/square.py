775254ba2bd7936ed9209b241f38b405752d7d32
Elnur <elnurra@code.edu.az>
2024-02-17 10:13:44 -0800

    error-fix: fixed 2 blank lines on square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Defines a Square class that inherits from Rectangle."""
 
+
 from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
     """Square class that inherits from Rectangle."""
