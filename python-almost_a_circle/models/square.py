2d396f50ca2143604258fd1d3add922f0e0e31cd
Elnur <elnurra@code.edu.az>
2024-02-17 10:10:24 -0800

    feat: added he class Square that inherits from Rectangle:

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle."""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes a Square instance."""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns a string representation of the Square instance."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+        )
