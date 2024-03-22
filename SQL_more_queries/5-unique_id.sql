20e079dec30fdde014c1f90477913053ac7827dd
elnurra <elnurra@code.edu.az>
2024-03-23 02:27:12 +0400

    feat: added a script that creates the table unique_id on MySQL server.

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- SQL - More queries, task 5. Unique ID
+-- Creates table `unique_id` with `id and `name` fields. DB in args.
+CREATE TABLE IF NOT EXISTS unique_id (
+       id INT DEFAULT 1 UNIQUE,
+       name VARCHAR(256)
+); 
