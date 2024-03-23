e7b58aa8ec0d046fe159df17576e23e8d727f294
elnurra <elnurra@code.edu.az>
2024-03-23 06:29:41 +0400

    feat: addded a script that lists all cities from the database hbtn_0e_4_usa

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-
-"""
-    a script that lists all cities from the database hbtn_0e_4_usa
-"""
+"""Lists states"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         password=argv[2], database=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT c.id, c.name, s.name\
-                 FROM states s, cities c\
-                 WHERE c.state_id = s.id\
-                 ORDER BY c.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
